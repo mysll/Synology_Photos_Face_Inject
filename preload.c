232d882a8777b8526219b168bd7de48af4782a3a
liangliang.shi <liangliang.shi@gmail.com>
2022-05-12 21:34:18 +0800

    Update preload.c

--- preload.c
+++ preload.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 long long _ZN9synophoto6plugin7network9IeNetwork11IsSupportedEv() {
-    printf("synophoto::plugin::network::IeNetwork::IsSupported return 0\n");
     return 0LL;
 }
 
