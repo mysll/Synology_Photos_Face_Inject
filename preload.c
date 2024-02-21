c6398f11d60accc69030120cd535b7b6887ef94c
liangliang.shi <liangliang.shi@gmail.com>
2024-02-21 14:37:19 +0800

    Update preload.c

--- preload.c
+++ preload.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+// patch libsynophoto-plugin-platform.so 
+long long _ZN9synophoto6plugin8platform15IsSupportedFaceEv() {
+    return 0LL;
+}
+
+// patch libsynophoto-plugin-model.so
 long long _ZN9synophoto6plugin7network9IeNetwork11IsSupportedEv() {
     return 0LL;
 }
